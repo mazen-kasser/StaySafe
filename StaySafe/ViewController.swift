7b5d35e1b6f74f7924f592c99d71b3f3b725d103
Mazen Kasser <mazen.kasser@anz.com>
2020-04-06 22:03:15 +1200

    Empty project

---
+++ StaySafe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StaySafe
+//
+//  Created by Mazen on 6/04/20.
+//  Copyright © 2020 iProgram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
