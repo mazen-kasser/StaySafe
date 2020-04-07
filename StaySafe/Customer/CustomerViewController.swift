4391e344a2fe5bb520d213b0c76771cecab1e704
Mazen Kasser <mazen.kasser@anz.com>
2020-04-07 20:04:10 +1200

    Empty views with UUID

---
+++ StaySafe/Customer/CustomerViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CustomerViewController.swift
+//  StaySafe
+//
+//  Created by Mazen on 7/04/20.
+//  Copyright © 2020 iProgram. All rights reserved.
+//
+
+import UIKit
+
+class CustomerViewController: UIViewController {
+ 
+    @IBOutlet weak var deviceIdentifierLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.deviceIdentifierLabel.text = UUID().uuidString
+    }
+}
