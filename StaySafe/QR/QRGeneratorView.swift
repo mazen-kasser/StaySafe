50c965ac0c53ab0e540856d31d9e45adc5b6f616
Mazen Kasser <mazen.kasser@anz.com>
2020-04-11 12:56:24 +1200

    Clean up the QR code

--- StaySafe/QR/QRGeneratorView.swift
+++ StaySafe/QR/QRGeneratorView.swift
@@ -11,17 +11,40 @@ import UIKit
 enum QRGenerator {
     
     static func generateQRCode(from string: String) -> UIImage? {
-        let data = string.data(using: String.Encoding.ascii)
+        // Get data from the string
+        guard let scaledQrImage = string.qrImage else { return nil }
+        
+        // Do some processing to get the UIImage
+        let context = CIContext()
+        guard let cgImage = context.createCGImage(scaledQrImage, from: scaledQrImage.extent) else { return nil }
+        
+        return UIImage(cgImage: cgImage)
+    }
+}
 
-        if let filter = CIFilter(name: "CIQRCodeGenerator") {
-            filter.setValue(data, forKey: "inputMessage")
-            let transform = CGAffineTransform(scaleX: 3, y: 3)
+fileprivate extension String {
+    
+    var qrImage: CIImage? {
+        guard let qrFilter = CIFilter(name: "CIQRCodeGenerator") else { return nil }
+        
+        let qrData = data(using: String.Encoding.ascii)
+        qrFilter.setValue(qrData, forKey: "inputMessage")
 
-            if let output = filter.outputImage?.transformed(by: transform) {
-                return UIImage(ciImage: output)
-            }
-        }
+        let qrTransform = CGAffineTransform(scaleX: 10, y: 10)
+        return qrFilter.outputImage?.transformed(by: qrTransform)
+    }
+}
 
-        return nil
+fileprivate extension CIImage {
+    
+    /// Combines the current image with the given image centered.
+    func combined(with image: CIImage? = nil) -> CIImage? {
+        guard let image = image,
+            let combinedFilter = CIFilter(name: "CISourceOverCompositing") else { return nil }
+        
+        let centerTransform = CGAffineTransform(translationX: extent.midX - (image.extent.size.width / 2), y: extent.midY - (image.extent.size.height / 2))
+        combinedFilter.setValue(image.transformed(by: centerTransform), forKey: "inputImage")
+        combinedFilter.setValue(self, forKey: "inputBackgroundImage")
+        return combinedFilter.outputImage!
     }
 }
