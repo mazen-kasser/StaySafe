6c41cb29abb61a3f858fd68176f13be8829bf8b1
Mazen Kasser <mazen.kasser@anz.com>
2020-04-13 17:29:06 +1200

    Add footer to the bottom

--- StaySafe/Device/Controller/ContactUsViewController.swift
+++ StaySafe/Device/Controller/ContactUsViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ContactUsViewController: UITableViewController {
 
+    @IBOutlet private weak var footerNoteLabel: UILabel!
     @IBOutlet private weak var submitButton: UIBarButtonItem!
     @IBOutlet private weak var contactNameTextField: CATextField!
     @IBOutlet private weak var mobileNumberTextField: CATextField!
@@ -37,6 +38,44 @@ class ContactUsViewController: UITableViewController {
             mobileNumberTextField.isValid(true)
     }
     
+    override open func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // must be called at viewDidAppear to set footer based on screen vs content size
+        reloadFooterView()
+    }
+    
+    private func reloadFooterView() {
+        tableView.tableFooterView = nil
+        
+        // create view
+        footerNoteLabel.frame.size.width = tableView.bounds.width
+        footerNoteLabel.frame.size.height = tableView.bounds.height - tableView.contentSize.height - 100 // bar height
+        
+        // add view
+        let footerView = UIView(frame: footerNoteLabel.frame)
+        footerView.addSameSizedSubview(footerNoteLabel)
+        
+        tableView.tableFooterView = footerView
+        // adjust inset
+        tableView.clipsToBounds = false
+    }
+    
+}
+
+extension ContactUsViewController {
+    
+    override open func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        self.tableView.tableFooterView?.isHidden = true
+        coordinator.animateAlongsideTransition(in: nil, animation: nil, completion: {[weak self] _ in
+            guard let self = self else { return }
+            // required for the iPad to reload receipt footer when orientation change
+            self.reloadFooterView()
+            self.tableView.tableFooterView?.isHidden = false
+        })
+    }
+    
 }
 
 extension ContactUsViewController: UITextFieldDelegate {
@@ -48,5 +87,23 @@ extension ContactUsViewController: UITextFieldDelegate {
     
     func textFieldDidEndEditing(_ textField: UITextField) {
         submitButton.isEnabled = isScreenValid()
+    }
+}
+
+private extension UIView {
+    /// Add subView to self, with edge insets.
+    func addSameSizedSubview(_ subView: UIView,
+                                    edgeInsets: UIEdgeInsets = .zero) {
+        addSubview(subView)
+        insetSubview(subView, edgeInsets: edgeInsets)
+    }
+    
+    /// Inset subView to self, by edge insets.
+    func insetSubview(_ subView: UIView, edgeInsets: UIEdgeInsets) {
+        subView.translatesAutoresizingMaskIntoConstraints = false
+        subView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: edgeInsets.left).isActive = true
+        trailingAnchor.constraint(equalTo: subView.trailingAnchor, constant: edgeInsets.right).isActive = true
+        subView.topAnchor.constraint(equalTo: topAnchor, constant: edgeInsets.top).isActive = true
+        bottomAnchor.constraint(equalTo: subView.bottomAnchor, constant: edgeInsets.bottom).isActive = true
     }
 }
