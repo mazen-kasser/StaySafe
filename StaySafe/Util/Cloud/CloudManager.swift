8f8dca2ca5a4c389e1dd3185573ae09970fafb5d
Mazen Kasser <mazen.kasser@anz.com>
2020-04-29 13:16:09 +1200

    Silent the crash when notification permission is disabled

--- StaySafe/Util/Cloud/CloudManager.swift
+++ StaySafe/Util/Cloud/CloudManager.swift
@@ -9,17 +9,15 @@ class CloudManager {
     
     private static let database = CKContainer.default().publicCloudDatabase
     
-    static func checkin(with deviceToken: String = UserDefaults.standard.deviceToken!, merchantName: String) {
+    static func checkin(merchantName: String) {
         let checkinRecord = CKRecord(recordType: "Checkins")
-        checkinRecord["deviceID"] = deviceToken as CKRecordValue
         checkinRecord["merchantName"] = merchantName as CKRecordValue
         
         save(checkinRecord)
     }
     
-    static func report(with deviceToken: String = UserDefaults.standard.deviceToken!, fullName: String, mobileNumber: String) {
+    static func report(fullName: String, mobileNumber: String) {
         let reportRecord = CKRecord(recordType: "Reports")
-        reportRecord["deviceID"] = deviceToken as CKRecordValue
         reportRecord["fullName"] = fullName as CKRecordValue
         reportRecord["mobileNumber"] = mobileNumber as CKRecordValue
         
@@ -27,6 +25,10 @@ class CloudManager {
     }
     
     private static func save(_ record: CKRecord) {
+        // TODO: save offline only at this stage
+        guard let deviceToken = UserDefaults.standard.deviceToken else { return }
+        
+        record["deviceID"] = deviceToken as CKRecordValue
         database.save(record) { record, error in
             // TODO: retry again when you have internet connection by flagging the checkin with `offline`
             // or return pass or error status to notify the user accordingly
